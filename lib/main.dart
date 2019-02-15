06355de1e84cb55415720b6ee14319d0bdee845e
taioo <thaer.tai@gmail.com>
2019-02-15 19:25:56 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,13 @@ class MyApp extends StatelessWidget {
       // debugShowMaterialGrid: true,
       theme: ThemeData(
           brightness: Brightness.light,
+<<<<<<< HEAD
           primarySwatch: Colors.deepOrange,
           accentColor: Colors.deepPurple),
+=======
+          primarySwatch: Colors.red,
+          accentColor: Colors.cyan[600]),
+>>>>>>> parent of 86df633... add login
       home: HomePage(),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
+
 import '../product_manager.dart';
+
+
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('EasyList'),
-      ),
-      body: ProductManager(),
-    );
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManeger(),
+      );
   }
 }
