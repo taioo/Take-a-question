e1a709efa9f23b949184a37f29a71e26dda05897
taioo <thaer.tai@gmail.com>
2019-02-15 13:14:15 +0100

    t

--- lib/product_controll.dart
+++ lib/product_controll.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ProductControll extends StatelessWidget{
-
+class ProductControll extends StatelessWidget {
   final Function addProduct;
 
   ProductControll(this.addProduct);
 
-
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              addProduct('Swweeeeeeeets');
-            },
-            child: Text('add product'),
-          );
+      color: Theme.of(context).primaryColor,
+      onPressed: () {
+        addProduct('Swweeeeeeeets');
+      },
+      child: Text('add product'),
+    );
   }
-  
 }
